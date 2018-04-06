382607e3e972ed43a6880f4ce16cda6c56b9cd55
zmoore <zmoore@usgs.gov>
2018-04-06 12:09:42 -0500

    Modified the Dockerfile to expect an Rserve.pwd matching the format expected by Rserve itself to be mounted instead of rservePassword.txt in order to allow for reusability of configuration files.

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,6 +20,11 @@ if [ -d "${CERT_IMPORT_DIRECTORY}" ]; then
   done
 fi
 
-java -Djava.security.egd=file:/dev/./urandom -DkeystorePassword=$KEYSTORE_PASSWORD -DrserveServicePassword=$(cat ${RSERVE_SERVICE_PASSWORD_PATH}) -jar app.jar $@
+if [ -n "${RSERVE_PWD_PATH}" ]; then
+  RSERVE_USER=$(cat ${RSERVE_PWD_PATH} | awk '{print $1;}')
+  RSERVE_PASSWORD=$(cat ${RSERVE_PWD_PATH} | awk '{print $2;}')
+fi
+
+java -Djava.security.egd=file:/dev/./urandom -DkeystorePassword=$KEYSTORE_PASSWORD -DrserveUser=$RSERVE_USER -DrservePassword=$RSERVE_PASSWORD -jar app.jar $@
 
 exec env "$@"
