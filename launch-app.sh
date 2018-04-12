e1938e90325b730e7be3589e05a2cc81fcdf0fc8
“mhines-usgs” <mhines@usgs.gov>
2018-04-12 13:41:56 -0500

    AQCU-1619: incorrect keystorePassword reference

--- launch-app.sh
+++ launch-app.sh
@@ -6,6 +6,6 @@ if [ -n "${RSERVE_PWD_PATH}" ]; then
   RSERVE_PASSWORD=$(cat ${RSERVE_PWD_PATH} | awk '{print $2;}')
 fi
 
-java -Djava.security.egd=file:/dev/./urandom -DkeystorePassword=$KEYSTORE_PASSWORD -DrserveUser=$RSERVE_USER -DrservePassword=$RSERVE_PASSWORD -jar app.jar $@
+java -Djava.security.egd=file:/dev/./urandom -DkeystorePassword=$keystorePassword -DrserveUser=$RSERVE_USER -DrservePassword=$RSERVE_PASSWORD -jar app.jar $@
 
 exec env "$@"
